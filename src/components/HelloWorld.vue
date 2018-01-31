cfaf301c4b3790c01ec228262b31b6917fb6d59f
Théo DRAPPIER <theo.drappier85@gmail.com>
2018-01-31 18:43:43 +0100

    Add routing + Add new page with todolist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -80,37 +80,18 @@
         </a>
       </li>
     </ul>
-    <ul>
-      <li v-for="todo in todosList" :key='todo.id'>
-        {{ todo.id }} => {{ todo.name }}
-      </li>
-    </ul>
+    <router-link to="/todos">Take a look at your todos list !</router-link>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your App',
-      todosList: []
+      msg: 'Welcome to Your App'
     }
-  },
-  created () {
-    axios.get('http://127.0.0.1:8000/api/tests')
-      .then(response => {
-        this.todosList = response.data
-      }
-      )
-      .catch(e => {
-        console.log(e)
-      }
-      )
   }
-
 }
 </script>
 
---
+++ src/components/TodosList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="todo in todosList" v-bind:key='todo.id'>
+        {{ todo.id }} => {{ todo.name }}
+      </li>
+    </ul>
+    <router-link to="/">Back</router-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'TodosList',
+  data () {
+    return {
+      msg: 'Todos List',
+      todosList: []
+    }
+  },
+  created () {
+    axios.get('http://127.0.0.1:8000/api/tests')
+      .then(response => {
+        this.todosList = response.data
+      }
+      )
+      .catch(e => {
+        console.log(e)
+      }
+      )
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
