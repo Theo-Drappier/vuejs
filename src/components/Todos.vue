8f220500f5b66e1a1be25346b91fe602b534e796
Florian Bezagu <florian@bezagu.com>
2018-02-04 16:41:04 +0100

    First commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import api from '../api';
+
+export default {
+  name: `Todos`,
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    api.get(`/todos`)
+      .then((response) => {
+        this.todos = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  // display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
